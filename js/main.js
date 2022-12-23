25eb30f70020236540f79318304dd67b619ae0da
ENNA-Egor <116068479+ENNA-Egor@users.noreply.github.com>
2022-12-23 11:46:29 +0300

    Попытка заставить созданную кнопку кликать.

--- js/main.js
+++ js/main.js
@@ -11,18 +11,30 @@
 const apElement = document.querySelector('#content');
 
 const newElement = document.createElement('div');
+const newElement2 = document.createElement('button');
 newElement.innerHTML = `Привет, всё получилось`;
-console.log (newElement);
+newElement2.innerHTML ='Эта кнопка создана JS';
+newElement2.id  = "btn_test2";
+console.log (newElement2);
 console.log (apElement);
-
-
 
 (function() {
    'use strict';
    var csInterface = new CSInterface();
 
-   document.getElementById ('btn_test').addEventListener('click', function(){
-     apElement.after(newElement);
+    document.getElementById ('btn_test').addEventListener('click', function(){
+     apElement.append(newElement);
+    apElement.prepend(newElement2);
       console.log("Ok");
+   });
+}());
+
+(function() {
+   'use strict';
+   var csInterface = new CSInterface();
+
+   document.getElementById ('btn_test2').addEventListener('click', function(){
+    console.log ("Кнопка работает!!!!");
+   alert ("Кнопка работает!!!!");
    });
 }());
