ea364c024d07f35f3ca628c5aedd0e3da81e842c
ENNA-Egor <116068479+ENNA-Egor@users.noreply.github.com>
2022-12-23 09:56:10 +0300

    Update main.js
    Создание текстового поля через кнопку

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-(function() {
+/*(function() {
    'use strict';
    var csInterface = new CSInterface();
 
    document.getElementById ('btn_test').addEventListener('click', function(){
       csInterface.evalScript('sayHello()');
+      console.log("Ok");
+   });
+}());*/
+
+const apElement = document.querySelector('#content');
+
+const newElement = document.createElement('div');
+newElement.innerHTML = `Привет, всё получилось`;
+console.log (newElement);
+console.log (apElement);
+
+
+
+(function() {
+   'use strict';
+   var csInterface = new CSInterface();
+
+   document.getElementById ('btn_test').addEventListener('click', function(){
+     apElement.after(newElement);
       console.log("Ok");
    });
 }());
