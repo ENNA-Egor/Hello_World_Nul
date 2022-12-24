b9ceee13e412437d58e2f25e6fe29fda9b024438
ENNA-Egor <e.yudakov@gmail.com>
2022-12-24 20:46:14 +0300

    Новое изменение

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-/*(function() {
+(function() {
    'use strict';
    var csInterface = new CSInterface();
-
    document.getElementById ('btn_test').addEventListener('click', function(){
       csInterface.evalScript('sayHello()');
       console.log("Ok");
    });
-}());*/
+}());
 
 const apElement = document.querySelector('#content');
 
@@ -15,16 +14,19 @@ const newElement2 = document.createElement('button');
 newElement.innerHTML = `Привет, всё получилось`;
 newElement2.innerHTML ='Эта кнопка создана JS';
 newElement2.id  = "btn_test2";
+newElement2.className  = "btn";
 console.log (newElement2);
 console.log (apElement);
+apElement.prepend(newElement2);
+
 
 (function() {
    'use strict';
    var csInterface = new CSInterface();
 
-    document.getElementById ('btn_test').addEventListener('click', function(){
+    document.getElementById ('btn_test3').addEventListener('click', function(){
      apElement.append(newElement);
-    apElement.prepend(newElement2);
+    //apElement.prepend(newElement2);
       console.log("Ok");
    });
 }());
@@ -32,7 +34,7 @@ console.log (apElement);
 (function() {
    'use strict';
    var csInterface = new CSInterface();
-
+ //  apElement.prepend(newElement2);
    document.getElementById ('btn_test2').addEventListener('click', function(){
     console.log ("Кнопка работает!!!!");
    alert ("Кнопка работает!!!!");
